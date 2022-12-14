e4e36bb9fc04974c52af0fdc1ce631611a815f62
Ajay Kumar D <115684961+AjayGowdaaaa@users.noreply.github.com>
2022-12-14 09:54:56 +0530

    cntrl cls upd

--- src/main/java/com/ebs/controller/EBS_Controller.java
+++ src/main/java/com/ebs/controller/EBS_Controller.java
@@ -13,15 +13,6 @@ import com.ebs.entity.User;
 import com.ebs.model.UserModel;
 import com.ebs.service.UserService;
 
-/*
- * 29/11/2022
- * Admin acces is working fine
- * default pages also working fine
- *  NOTE-
- *  1- Register page not working
- *  2- User access is not working
- */
-
 @RestController
 @RequestMapping("/ebs")
 public class EBS_Controller {
@@ -29,7 +20,7 @@ public class EBS_Controller {
 	@Autowired
 	private UserService service;
 
-	
+
 	@GetMapping("/wc")
 	public String ebs() {
 		return "Welcome to EBS in EBS Controller";
@@ -46,21 +37,119 @@ public class EBS_Controller {
 	public String admin() {
 		return "Admin Method Calling";
 	}
-	
+
 	@PostMapping("/register")
 	public ResponseEntity<?> register( @RequestBody UserModel userModel) {
 		System.out.println("Register Method Calling");
 		User user = service.register(userModel);
 		return new ResponseEntity<User>(user, HttpStatus.CREATED);
 	}
-	
+
 	@GetMapping("/user/{userName}")
 	public ResponseEntity<?> getUserByUserName(@PathVariable("userName") String userName) {	
 		User user = service.getUserByUserName(userName);
 		return new ResponseEntity<User>(user, HttpStatus.ACCEPTED);
-
-
 	}
+
+	/*
+	 * Converted a servlet into Controller
+	 * NOTE = Need to do Write Business Logic and Need to change get and post method
+	 * 
+	 */
+	
+	@GetMapping("/addPrograms")
+	public String addPrograms() {
+		return "/servlet/addPrograms";
+	}
+	@GetMapping("/Archiveenv")
+	public String Archiveenv() {
+		return "/servlet/Archiveenv";
+	}
+	@GetMapping("/ArchiveEnvironment")
+	public String ArchiveEnvironment() {
+		return "/servlet/ArchiveEnvironment";
+	}
+	@GetMapping("/GrantSys")
+	public String GrantSys() {
+		return "/servlet/GrantSys";
+	}
+	@GetMapping("/datapath")
+	public String datapath() {
+		return "/servlet/datapath";
+	}
+	@GetMapping("/ArchiveDatabase")
+	public String ArchiveDatabase() {
+		return "/servlet/ArchiveDatabase";
+	}
+	@GetMapping("/ArchiveFileInfo")
+	public String ArchiveFileInfo() {
+		return "/servlet/ArchiveFileInfo";
+	}
+	@GetMapping("/buildTree")
+	public String buildTree() {
+		return "/servlet/buildTree";
+	}
+	@GetMapping("/changePassword")
+	public String changePassword() {
+		return "/servlet/changePassword";
+	}
+	@GetMapping("/checkUser")
+	public String checkUser() {
+		return "/servlet/checkUser";
+	}
+	@GetMapping("/configuration")
+	public String configuration() {
+		return "/servlet/configuration";
+	}
+	@GetMapping("/DetailReport")
+	public String DetailReport() {
+		return "/servlet/DetailReport";
+	}
+	@GetMapping("/DBProfile")
+	public String DBProfile() {
+		return "/servlet/DBProfile";
+	}
+	@GetMapping("/Diagnostics")
+	public String Diagnostics() {
+		return "/servlet/Diagnostics";
+	}
+	@GetMapping("/Group")
+	public String Group() {
+		return "/servlet/Group";
+	}
+	@GetMapping("/Job")
+	public String Job() {
+		return "/servlet/Job";
+	}
+	@GetMapping("/logon")
+	public String logon() {
+		return "/servlet/logon";
+	}
+	@GetMapping("/logout")
+	public String logout() {
+		return "/servlet/logout";
+	}
+	@GetMapping("/programs")
+	public String programs() {
+		return "/servlet/programs";
+	}
+	@GetMapping("/purgeJobs")
+	public String purgeJobs() {
+		return "/servlet/purgeJobs";
+	}
+	@GetMapping("/User")
+	public String User() {
+		return "/servlet/User";
+	}
+	@GetMapping("/viewJobs")
+	public String viewJobs() {
+		return "/servlet/viewJobs";
+	}
+	@GetMapping("/webdav")
+	public String webdav() {
+		return "/servlet/webdav";
+	}
+
 
 
 }
