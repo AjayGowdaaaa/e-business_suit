4392f9032bcca25b950439727b8264dd2efae14a
Sanjay Thimmalapura Prasanna <SPrasanna@ESTUATE.COM>
2022-12-29 14:15:24 +0530

    task2

--- src/main/java/com/ebs/service/UserService.java
+++ src/main/java/com/ebs/service/UserService.java
@@ -1,33 +1,75 @@
 package com.ebs.service;
 
-
-import java.util.List;
-
-import com.ebs.entity.Assigned_Programs;
-//import com.ebs.entity.ChangePassword_Optim;
-import com.ebs.entity.GroupData;
-//import com.ebs.entity.Programs;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Service;
 import com.ebs.entity.User;
-import com.ebs.model.UserModel;
+import com.ebs.exception.BusinessException;
+import com.ebs.repository.UserRepository;
 
-public interface UserService {
+@Service
+public class UserService implements UserServiceInterface {
 
-	User register(UserModel userModel);
+	@Autowired
+	UserRepository userRepo;
 
-	User getUserByUserName(String userName);
-	
-	
-	//Group Creation
-	GroupData newGroup(GroupData groupData);
-	
-	GroupData getGroupCreationByGroupName(String groupName);
-	void deleteGroupbyName(String groupName);
-	GroupData modifyGroup(String groupName, GroupData groupData);
-	User changePassword(String userName, User user) throws Exception;
-//Assigned Programs
-	Assigned_Programs AssignedPrograms(Assigned_Programs programs) throws Throwable;
-	List  assignGroups( GroupData groupData) throws Exception, Throwable;
-	List  getPrograms( GroupData groupData) throws Throwable;
-	List  get_Particular_Program( String groupName) throws Exception;
+
+	@Override
+	public User register(User user) {
+		System.out.println("Register Method Calling");
+		if (!(userRepo.findByEmail(user.getEmail())==null)) {
+			throw new DuplicateKeyException("Email ID Already Exsists");
+		}else if (!(userRepo.findByUserName(user.getUserName())==null)) {
+			throw new DuplicateKeyException("UserName Already Exsists");
+		}else {
+			user.setEmail(user.getEmail().toLowerCase());
+			user.setUserName(user.getUserName().toLowerCase());
+			user.setRole("ROLE_"+user.getRole().toUpperCase());	
+			userRepo.save(user);	
+		}
+		return user;
+	}
+	@Override
+	public User update(Long id,User user) {
+		User exsistingUser = userRepo.findById(id).orElseThrow(() -> new BusinessException("User does not exsits in Repository", "Please Enter valid profileName"));
+		exsistingUser.setUserName(user.getUserName());
+		exsistingUser.setPassword(user.getPassword());
+		exsistingUser.setEmail(user.getEmail());
+		exsistingUser.setRole("ROLE_"+user.getRole().toUpperCase());
+		try {
+			userRepo.save(exsistingUser);
+
+		} catch (Exception e) {
+			throw new BusinessException("update user ","Failed to Update User " + e.getMessage());	
+		}
+		return exsistingUser;
+	}
+
+
+	@Override
+	public void delete(Long id) {
+		User exsistingUser = userRepo.findById(id).orElseThrow(() -> new BusinessException("User does not exsits in Repository", "Please Enter valid profileName"));
+		userRepo.delete(exsistingUser);	
+	}
+
+	@Override
+	public User getUserById(Long id) {
+		User user = userRepo.findById(id).get();
+		return user;
+	}
+
+	@Override
+	public User getUserByUserName(String userName) {
+		User user = userRepo.findByUserName(userName);
+		return user;
+	}
+
+
+	@Override
+	public User getUserByEmail(String email) {
+		User user = userRepo.findByEmail(email);
+		return user;
+	}
+
 
 }
