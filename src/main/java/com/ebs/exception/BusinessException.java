031337909da1c7ae6737b0f619b721edccf14834
Ajay Kumar D <115684961+AjayGowdaaaa@users.noreply.github.com>
2023-01-04 12:14:54 +0530

    DBP Update & Configure NAA

--- src/main/java/com/ebs/exception/BusinessException.java
+++ src/main/java/com/ebs/exception/BusinessException.java
@@ -3,8 +3,6 @@ package com.ebs.exception;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@Data
-@AllArgsConstructor
 public class BusinessException extends RuntimeException{
 	
 private static final long serialVersionUID = 7689723270514874053L;
@@ -12,4 +10,26 @@ private static final long serialVersionUID = 7689723270514874053L;
 	
 	private String errorCode;
 	private String errorMessage;
+	public String getErrorCode() {
+		return errorCode;
+	}
+	public void setErrorCode(String errorCode) {
+		this.errorCode = errorCode;
+	}
+	public String getErrorMessage() {
+		return errorMessage;
+	}
+	public void setErrorMessage(String errorMessage) {
+		this.errorMessage = errorMessage;
+	}
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	public BusinessException(String errorCode, String errorMessage) {
+		super();
+		this.errorCode = errorCode;
+		this.errorMessage = errorMessage;
+	}
+	
+	
 }
